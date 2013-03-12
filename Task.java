82e56a20a770ea952f5ed2bfdabc8ae8ea962897
Syunta Kitagawa <kitagawa@timedia.co.jp>
2013-03-12 18:08:32 +0900

    Prepare the Task model まで終了

---
+++ Task.java
@@ -0,0 +1,20 @@
+package models;
+
+import java.util.*;
+
+public class Task {
+	
+	public Long id;
+	public String label;
+	
+	public static List<Task> all() {
+		return new ArrayList<task>();
+	}
+	
+	public static void create(Task task) {
+	}
+	
+	public static void delete(Long id) {
+	}
+	
+}
