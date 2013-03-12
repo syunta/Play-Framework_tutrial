f4d1efbed6cbcfc9c148e9f49f8c85f3b3c87ef4
Syunta Kitagawa <kitagawa@timedia.co.jp>
2013-03-12 17:52:47 +0900

    Preparing the application まで終了

--- Application.java
+++ Application.java
@@ -6,9 +6,22 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok("Hello world");
-  }
-  
+
+	public static Result index() {
+		return redirect(routes.Application.tasks());
+	}
+
+	
+	public static Result tasks() {
+		return TODO;
+	}
+
+	public static Result newTask() {
+		return TODO;	  
+	} 
+
+	public static Result deleteTask(Long id) {
+		return TODO;
+	}
+
 }
